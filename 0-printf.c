aa8419005c22714fc216ffa9ba0287cbc67e844f
root <iamconfidence5@gmail.com>
2023-11-12 01:06:29 -0800

    percent

--- 0-printf.c
+++ 0-printf.c
@@ -16,22 +16,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	}
 	va_start(list_of_args, format);
-	cha_print = format_string(format, args);
-	va_end(args);
-	return (cha_print);
-}
-/**
- * _printf - a function for processing the string
- * @format: format list
- * Return: Number of characters
- */
 
-int _printf(const char *format, ...)
-{
-	int cha_print = 0;
-	va_list list_of_args;
-
-	va_start(list_of_args, format);
 
 	while (*format)
 	{

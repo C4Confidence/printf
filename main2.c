964f6ba8efe79c44dca4162da35e7b5cb736ad88
edaoudihicham <edaoudihicham@gmail.com>
2023-11-14 00:43:49 +0100

    test

--- main2.c
+++ main2.c
@@ -2,13 +2,10 @@
 
 int main(void)
 {
-	int len;
-    int len2;
+
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-	_printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
     return (0);
 }

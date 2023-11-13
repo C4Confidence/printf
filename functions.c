7fce2c1178140390096912ffd465797b6ac1b3ad
edaoudihicham <edaoudihicham@gmail.com>
2023-11-13 23:47:41 +0100

    functions

--- functions.c
+++ functions.c
@@ -9,21 +9,21 @@ int print_percent(void)
 
 int print_char(char c)
 {
-    write(1, &c, 1);
-    return (1);
+	write(1, &c, 1);
+	return (1);
 }
 
 void print_string(char *str, int *count)
 {
-    int i;
+	int i;
 
-    i = 0;
-    if (!str)
-        str = "(null)";
-    while (str[i])
-    {
-        i++;
-        *count += 1;
-    }
-    write(1, str, i);
+	i = 0;
+	if (!str)
+		str = "(null)";
+	while (str[i])
+	{
+		i++;
+		*count += 1;
+	}
+	write(1, str, i);
 }

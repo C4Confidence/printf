1e082e5b315eb625e530fd739b0945c23f5941d6
edaoudihicham <edaoudihicham@gmail.com>
2023-11-13 23:33:12 +0100

    count

--- _printf.c
+++ _printf.c
@@ -7,8 +7,12 @@
  * Return: Printed Characters c s %
 */
 
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
     int i;
     int count;
     va_list arg_list;
+
+	i = 0;
+    count = 0;
+    va_start(arg_list, format);

beca7b9828fe903871e1031156f35b4a46e4c141
edaoudihicham <edaoudihicham@gmail.com>
2023-11-14 16:49:45 +0100

    printffix

--- _printf.c
+++ _printf.c
@@ -15,6 +15,8 @@ int _printf(const char *format, ...)
 
 	i = 0;
 	count = 0;
+	if (write(1, "", 1) < 0)
+		return (-1);
 	va_start(arg_list, format);
 	while (format[i])
 	{
@@ -25,12 +27,13 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == 'c')
 				count += print_char(va_arg(arg_list, int));
-			else if (format[i] == '%')
-				count += print_percent();
 			else if (format[i] == 's')
 				print_string(va_arg(arg_list, char *), &count);
+			else
+				count += print_char(format[i]);
 		}
-		i++;
+		if (format[i])
+			i++;
 	}
 	va_end(arg_list);
 	return (count);

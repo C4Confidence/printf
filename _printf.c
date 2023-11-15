33de29f780e5c0d0e067b50d3212b4f1be16fa2f
root <iamconfidence5@gmail.com>
2023-11-14 22:50:00 -0800

    yes

--- _printf.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/**
- * _printf - Prints characters
- * A group project by Aigbedion Confidence and Hicham Daoudi
- * Return: Printed Characters c s %
-*/
-
-int _printf(const char *format, ...);
--- main.h
+++ main.h
@@ -4,7 +4,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
+typedef struct format
+{
+	char *id;
+	int  (*f)();
+} match;
+
+
+int _strlen(char *str);
+int _strlenc(const char *str);
+int printf_string(va_list val);
+int printf_char(va_list val);
+int _putchar(char c);
+int print_37(void);
+int print_int(va_list args);
+int print_bina(va_list val);
+int print_d(va_list args);
 int _printf(const char *format, ...);
 int binary (int num);
 int octal(int num);
